5fb9c826449e0246c880e88b65375adf91542446
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2026-05-13 11:51:53 +0530

    Creating and rendering lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,12 @@
-// import { Fragment } from "react";
-
 function ListGroup() {
+  const items = ["Hyderabad", "Bengaluru", "Mumbai", "Pune", "Chennai"];
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
