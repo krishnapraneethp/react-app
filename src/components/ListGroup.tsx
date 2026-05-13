2d57d0e1619d1b00ab96d63b3a4ddf68b8771477
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2026-05-13 11:59:21 +0530

    creating a conditional rendering using a ternary operator

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,17 +2,11 @@ function ListGroup() {
   let items = ["Hyderabad", "Bengaluru", "Mumbai", "Pune", "Chennai"];
   items = [];
 
-  if (items.length == 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No items found</p>
-      </>
-    );
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
+        {items.length == 0 ? <p>No items found</p> : null}
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
