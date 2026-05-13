62ac765094cc59ab230342fd6002d6cb3deba7c0
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2026-05-13 12:14:33 +0530

    Condtiional Rendering using if statement, ternary operator and functions

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,11 +2,29 @@ function ListGroup() {
   let items = ["Hyderabad", "Bengaluru", "Mumbai", "Pune", "Chennai"];
   items = [];
 
+  const ifMessage = () => {
+    if (items.length == 0)
+      return (
+        <>
+          <h1>List</h1>
+          <p>No items found</p>
+        </>
+      );
+  };
+
+  const ternarymessage = items.length == 0 ? <p>No items found</p> : null;
+
+  const funcMessage = (message: string) => {
+    return items.length == 0 ? <p>{message}</p> : null;
+  };
+
+  const condMessage = items.length == 0 && <p>No items found</p>;
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.length == 0 ? <p>No items found</p> : null}
+        {condMessage}
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
