c04363cb7d85e28fb5e7dd4f481f527b9aa8a458
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2026-05-13 08:40:54 +0530

    creating a react component

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    // babeljs.io/repl - transform JSX to JavaScript
+    const name = "Krishna";
+    if(name) 
+        return <h1> Hello {name} </h1>;
+    return <h1> Hello World </h1>;
+}
+
+export default Message;
