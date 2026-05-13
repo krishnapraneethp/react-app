b240e49cddfdb3a81fd3d3e92db71839210f913b
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2026-05-13 12:57:28 +0530

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,32 +1,18 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Hyderabad", "Bengaluru", "Mumbai", "Pune", "Chennai"];
-  items = [];
 
-  const ifMessage = () => {
-    if (items.length == 0)
-      return (
-        <>
-          <h1>List</h1>
-          <p>No items found</p>
-        </>
-      );
-  };
-
-  const ternarymessage = items.length == 0 ? <p>No items found</p> : null;
-
-  const funcMessage = (message: string) => {
-    return items.length == 0 ? <p>{message}</p> : null;
-  };
-
-  const condMessage = items.length == 0 && <p>No items found</p>;
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {condMessage}
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, _index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
