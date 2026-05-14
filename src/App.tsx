4e70b79276cc8a06f95449b36391d2dc962a44d0
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2026-05-14 16:02:41 +0530

    Exercise - Showing an Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [isAlertVisible, setAlertVisible] = useState(false);
   return (
-    <Button color="primary" onClick={() => console.log("Clicked")}>
-      My New Button
-    </Button>
+    <>
+      <Button color="primary" onClick={() => setAlertVisible(true)}>
+        Popup Alert
+      </Button>
+      {isAlertVisible && (
+        <Alert
+          onClose={() => {
+            setAlertVisible(false);
+          }}
+        ></Alert>
+      )}
+    </>
   );
 }
 
